7d65710452406f2d04304118adc5435744cde5b8
Fazek <kristoffazekas34@gmail.com>
2024-12-21 16:46:28 +0100

    kezdő file-ok hozzáadása

---
+++ src/main/java/com/carparts/AutoFixHub/AutoFixHubApplication.java
@@ -0,0 +1,13 @@
+package com.carparts.AutoFixHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoFixHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoFixHubApplication.class, args);
+	}
+
+}
